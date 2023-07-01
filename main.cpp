b8fb964f8ca65fed7b75e49e1147eab6026a41ce
Slavchu <tothelove1@gmail.com>
2023-07-02 01:41:42 +0300

    potentiometer

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
+/*
+################################################
+#              02.07.2023                      #
+#         Welcome to my shitcode               #
+#           Written by Slavchoo                #
+################################################
+*/
 #include <iostream>
 #include <wiringPi.h>
 #include <chrono>
 #include <thread>
+
 int main(){
     wiringPiSetup();
     pinMode(7, OUTPUT);
+    pinMode(0, INPUT);
     bool statement = false;
     while(1){
-        digitalWrite(7, statement = !statement);
-        std::cout << statement << std::endl;
-        sleep(1);
-        digitalWrite(7, statement = !statement);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        digitalWrite(7, statement = !statement);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        digitalWrite(8, statement = !statement);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        analogWrite(analogRead(0), 7);
         
     }
 }
