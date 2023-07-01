f8289176aaa4ccc308186d43b2cbda31a1f64660
Slavchu <tothelove1@gmail.com>
2023-07-02 02:54:31 +0300

    what about digital?

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@ int main(){
     wiringPiSetup();
 
     pinMode(0, OUTPUT);
-
+    pinMode(2, INPUT);
     while(1){
         analogWrite( 0, 100);
 
         sleep(1);
         analogWrite( 0, 0);
         sleep(1);
-        std::cout <<(analogRead(2)) << std::endl;
+        std::cout <<(digitalRead(2)) << std::endl;
     }
 }
 
